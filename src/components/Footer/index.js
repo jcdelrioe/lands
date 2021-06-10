3bfaa1cb07abff73db96efae7e508b38c2ffd513
Juan Carlos Del Rio <jcdre@hotmail.com>
2021-06-10 18:55:48 -0500

    Changes in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-  return (
-    <div>
-      Este es el Footer
-    </div>
-  )
+	return <strong>Contacto: 099 149 1059 / 099 969 5192</strong>;
 }
