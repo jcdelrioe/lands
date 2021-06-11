3b512b5cbe10173c9830238455cf6264bf0dae71
Juan Carlos Del Rio <jcdre@hotmail.com>
2021-06-10 19:00:10 -0500

    React.lazy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "../Layout";
 import data from "Data/data.json";
-import Land from "components/Land";
+
+const Land = React.lazy(() => import("components/Land"));
 
 export default function Home() {
 	return (
 		<>
 			<Layout>
 				{data.data.map((item) => (
-					<Land
-						key={item.id}
-						id={item.id}
-						name={item.name}
-						squareM={item.squareM}
-						frente={item.frente}
-						fondo={item.fondo}
-						address={item.address}
-						images={item.images}
-						price={item.price}
-					/>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Land
+							key={item.id}
+							id={item.id}
+							name={item.name}
+							squareM={item.squareM}
+							frente={item.frente}
+							fondo={item.fondo}
+							address={item.address}
+							images={item.images}
+							price={item.price}
+						/>
+					</Suspense>
 				))}
 			</Layout>
 		</>
